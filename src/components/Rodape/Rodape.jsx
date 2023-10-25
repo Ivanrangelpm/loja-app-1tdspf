dae3c5e86e4bdbfe2676f5b7eb73de882328fb99
Ivanrangelpm <rm551005@fiap.com.br>
2023-10-25 11:18:21 -0300

    Trocando nome

--- src/components/Rodape/Rodape.jsx
+++ src/components/Rodape/Rodape.jsx
@@ -3,7 +3,7 @@ export default function Rodape() {
   return (
     <footer className="rodape">
         <div>
-            <p>Desenvolvido por <span>pf0670</span></p>
+            <p>Desenvolvido por <span>rm551005</span></p>
             <p>NEXT.js - React.js - tailwind</p>
         </div>
     </footer>
